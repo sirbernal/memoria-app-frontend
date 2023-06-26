b59dec0392101ce29fc21c247670224921e8b442
Cristian Bernal <cristian.bernal@sansano.usm.cl>
2023-06-26 21:07:13 +0000

    feat: Se actualiza test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renderiza Login', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Iniciar sesión/i);
   expect(linkElement).toBeInTheDocument();
 });
